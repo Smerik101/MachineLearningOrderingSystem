9ebf744a118d6ecc40f31ceec39d1600af1e61df
smerik101 <smerik111@gmail.com>
2025-07-04 16:36:36 +1000

    - All scripts modulated, ordering WIP.

--- Initialize.py
+++ Initialize.py
@@ -37,9 +37,34 @@ else:
 yesterday_date = yesterday_date.strftime("%d/%m/%Y")
 today_date = today_date.strftime("%d/%m/%Y")
 
+# Import Dataset
 df = pd.read_csv("TrainingSetNew.csv")
 df["Item Name"] = df["Item Name"].str.strip()
 df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%Y", errors="coerce")
 df.to_csv("backup.csv", index=False)
 df["Date"].head()
 df = df[df["Date"].notnull()]
+
+
+## Buffer Setup
+buffer_dict = {
+    "BUN REGULAR": 100,
+    "MUFFIN ENGLISH": 100,
+    "BUN 4 INCH": 80,
+    "BUN BIG MAC": 60,
+    "BUN MCCRISPY": 30,
+    "BUN GOURMET": 30,
+}
+
+## Order Delivery Setup
+order_days = (0, 3) #Days ordering is completed
+delivery_delay = 3 #Time for order to arrive
+
+delivery_day = []
+for i in range(len(order_days)):
+    x = order_days[i] + delivery_delay
+    if x > 6:
+        x = x - 7
+    delivery_day.append(x)
+
+
