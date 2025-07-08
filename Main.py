0aaa91aa582b24078f52082e8d3a3f6526a9cece
smerik101 <smerik111@gmail.com>
2025-07-08 22:24:38 +1000

    - GUI Fully implemented

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from stocktake import open_stocktake
 from ordering import open_ordering
+from Initialize import open_setup
 
 def stocktakerun():
     open_stocktake(window, frame)
@@ -8,19 +9,25 @@ def stocktakerun():
 def orderingrun():
     open_ordering(window, frame)
 
+def setuprun():
+    open_setup(window, frame)
+
 def mainmenu():
 
     label1 = Label(frame, text="Welcome!", font=("Arial", 12, "bold"))
     label1.grid(row = 1, column = 0, padx = 10, pady = 15, sticky= W)
 
     label2 = Label(frame, text="Please select an action to continue.", font=("Arial", 10))
-    label2.grid(row = 2, column = 0, padx= 10, columnspan=2)
+    label2.grid(row = 2, column = 0, padx= 10, columnspan=3)
 
     button1 = Button(frame, text = "Do Stocktake", font = ("Arial", 8, "bold"), command = stocktakerun)
-    button1.grid(row = 3, column = 0, padx = 10, pady= 20, sticky = W, columnspan= 2)
+    button1.grid(row = 3, column = 0, padx = 10, pady= 20, sticky = W, columnspan= 1)
 
     button2 = Button(frame, text = "Propose Order", font = ("Arial", 8, "bold"), command = orderingrun)
     button2.grid(row = 3, column = 1, padx = 10, pady = 20,  sticky = W,)
+
+    button3 = Button(frame, text="Setup", font=("Arial", 8, "bold"), command = setuprun)
+    button3.grid(row=4, column=0, padx=10, sticky=W, )
 
 # Initiate GUI upon program launch
 window = Tk()
